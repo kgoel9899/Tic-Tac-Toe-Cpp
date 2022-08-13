728b48eb99f33cbd5f9c6b7e48080213b214abd2
Kshitij Goel <kshitijg9899@gmail.com>
2022-08-14 02:02:50 +0530

    Refactored code, updated README

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-"# TTT-Project" 
+# Tic-Tac-Toe Game
+Developed using C++, a player can play it against the computer.
+Computer uses the minimax algorithm to predict its next best move and doesn't let you win!
--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,132 +1,114 @@
-//Tic-Tac-Toe Board
-#include<bits/stdc++.h>
-#define ll long long
+// Tic-Tac-Toe Game
+
+#include <bits/stdc++.h>
 using namespace std;
 
-char human = 'X';
-char computer = 'O';
-struct nextMove {
-    int nextR;
-    int nextC;
-};
-//tells whether there is tie or not (i.e. are there any moves left)
-int tie(char** board) {
+const static char human = 'X';
+const static char computer = 'O';
+const static char nothing = '-';
+vector<vector<char>> board;
+
+// tells whether there is tie or not (i.e. are there any moves left)
+bool Tie() {
     for(int i=0;i<3;i++) {
         for(int j=0;j<3;j++) {
-            if(board[i][j] == '-') {
-                return 0;
-            }
+            if(board[i][j] == '-') return false;
         }
     }
-    return 1;
+    return true;
 }
-//function that returns value according to who won the game
-int win(char** board) {
-    //row check
+
+// function that returns value according to who won the game
+int Win() {
+    // row check
     for(int i=0;i<3;i++) {
         char curr = board[i][0];
         if(curr == board[i][1] && curr == board[i][2]) {
-            if(curr == human) {
-                return 20;
-            } else if(curr == computer){
-                return -20;
-            }
+            if(curr == human) return 20;
+            else if(curr == computer) return -20;
         }
     }
-    //column check
+
+    // column check
     for(int j=0;j<3;j++) {
         char curr = board[0][j];
         if(curr == board[1][j] && curr == board[2][j]) {
-            if(curr == human) {
-                return 20;
-            } else if(curr == computer){
-                return -20;
-            }
+            if(curr == human) return 20;
+            else if(curr == computer) return -20;
         }
     }
-    //diagonal check
+
+    // diagonal check
     if(board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
-        if(board[0][0] == human) {
-            return 20;
-        } else if(board[0][0] == computer){
-            return -20;
-        }
+        if(board[0][0] == human) return 20;
+        else if(board[0][0] == computer) return -20;
     }
-    //diagonal check
+
+    // diagonal check
     if(board[2][0] == board[1][1] && board[1][1] == board[0][2]) {
-        if(board[2][0] == human) {
-            return 20;
-        } else if(board[2][0] == computer){
-            return -20;
-        }
+        if(board[2][0] == human) return 20;
+        else if(board[2][0] == computer) return -20;
     }
+
     return 0;
 }
-//returns the best possible score out of all the possible combinations (backtracking)
-int minimax(char** board, bool humanChance, int depth) {
-    int ans = win(board);
-    if(ans == 20) {
-        return ans - depth;
-    }
-    if(ans == -20) {
-        return ans + depth;
-    }
-    if(tie(board)) {
-        return 0;
-    }
-    if(humanChance) {
-        int maxi = INT_MIN;
+
+// returns the best possible score out of all the possible combinations (backtracking)
+int Minimax(bool human_chance, int depth) {
+    int ans = Win();
+    if(ans == 20) return ans - depth;
+    else if(ans == -20) return ans + depth;
+    else if(Tie()) return 0;
+
+    if(human_chance) {
+        int best = INT_MIN;
         for(int i=0;i<3;i++) {
             for(int j=0;j<3;j++) {
-                if(board[i][j] == '-') {
-                    board[i][j] = 'X';
-                    maxi = max(maxi, minimax(board, !humanChance, depth + 1));
-                    board[i][j] = '-';
+                if(board[i][j] == nothing) {
+                    board[i][j] = human;
+                    best = max(best, Minimax(!human_chance, depth + 1));
+                    board[i][j] = nothing;
                 }
             }
         }
-        return maxi;
+        return best;
     } else {
-        int mini = INT_MAX;
+        int best = INT_MAX;
         for(int i=0;i<3;i++) {
             for(int j=0;j<3;j++) {
-                if(board[i][j] == '-') {
-                    board[i][j] = 'O';
-                    mini = min(mini, minimax(board, !humanChance, depth + 1));
-                    board[i][j] = '-';
+                if(board[i][j] == nothing) {
+                    board[i][j] = computer;
+                    best = min(best, Minimax(!human_chance, depth + 1));
+                    board[i][j] = nothing;
                 }
             }
         }
-        return mini;
+        return best;
     }
 }
-//decides next optimal move for computer
-nextMove decideMove(char** board) {
-    nextMove result;
-    if(tie(board)) {
-        result.nextC = -1;
-        result.nextR = -1;
-        return result;
-    }
-    int mini = INT_MAX;
+
+// decides next optimal move for computer
+pair<int, int> DecideComputerMove() {
+    pair<int, int> result;
+    int best = INT_MAX;
     for(int i=0;i<3;i++) {
         for(int j=0;j<3;j++) {
-            if(board[i][j] == '-') {
-                board[i][j] = 'O';
-                int temp = minimax(board, 1, 0);
-                board[i][j] = '-';
-                if(temp < mini) {
-                    mini = temp;
-                    result.nextR = i;
-                    result.nextC = j;
+            if(board[i][j] == nothing) {
+                board[i][j] = computer;
+                int temp = Minimax(true, 0);
+                board[i][j] = nothing;
+                if(temp < best) {
+                    best = temp;
+                    result = {i, j};
                 }
             }
         }
     }
     return result;
 }
-//prints the current status of board
-void print(char** board) {
+
+// prints the current status of board
+void Print() {
     for(int i=0;i<3;i++) {
         for(int j=0;j<3;j++) {
             cout << board[i][j] << " ";
@@ -134,70 +116,60 @@ void print(char** board) {
         cout << endl;
     }
 }
+
 int main() {
-    //fast IO
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    char** board = new char*[3];
-    for(int i=0;i<3;i++) {
-        board[i] = new char[3];
-        for(int j=0;j<3;j++) {
-            board[i][j] = '-';
-        }
-    }
-    //position where human wants to place X
-    int row, col;
-    //initially it is human's turn
-    int humanCh = 1;
-    //loop until the game is tied or somebody has won
+    board.resize(3, vector<char>(3, nothing));
+
+    // initially it is human's turn
+    bool human_chance = 1;
+
+    // loop until the game is tied or somebody has won
     while(true) {
-        if(tie(board)) {
-            break;
-        }
-        if(win(board)) {
-            break;
-        }
-        cout << "The current status of the Tic-Tac-Toe Board is as follows:-" << endl;
+        if(Tie()) break;
+
+        if(Win()) break;
+
+        cout << "The current status of the Tic-Tac-Toe Board is as follows:" << endl;
         cout << endl;
-        print(board);
+
+        Print();
+
         cout << endl;
-        if(humanCh) {
+
+        if(human_chance) {
             cout << "Human's Turn (X)" << endl;
             cout << "Enter the row and column where you want to place X" << endl;
+
+            int row, col;
             cin >> row >> col;
-            if(board[row][col] != '-') {
+
+            if(board[row][col] != nothing) {
                 cout << endl;
                 cout << "Invalid Move, Try Again" << endl;
                 continue;
             }
-            board[row][col] = 'X';
+
+            board[row][col] = human;
             cout << endl;
-            humanCh = 0;
+            human_chance = 0;
         } else {
-            nextMove nMove = decideMove(board);
-            board[nMove.nextR][nMove.nextC] = 'O';
-            cout << "Computer optimally placed O at row " << nMove.nextR << " and column " << nMove.nextC << endl;
+            pair<int, int> move = DecideComputerMove();
+            board[move.first][move.second] = computer;
+            cout << "Computer optimally placed O at row = " << move.first << " and column = " << move.second << endl;
             cout << endl;
-            humanCh = 1;
+            human_chance = 1;
         }
     }
-    //printing the final board
-    cout << "The current status of the Tic-Tac-Toe Board is as follows:-" << endl;
+
+    // printing the final board
+    cout << "The final status of the Tic-Tac-Toe Board is as follows:" << endl;
     cout << endl;
-    print(board);
+    Print();
     cout << endl;
-    //printing the final result
-    if(win(board) > 0) {
-        cout << "Human Won!!" << endl;
-    } else if(win(board) < 0) {
-        cout << "Computer Won!!" << endl;
-    } else {
-        cout << "The match has been tied!!" << endl;
-    }
-    //deleting the dynamically allocated memory
-    for(int i=0;i<3;i++) {
-        delete [] board[i];
-    }
-    delete [] board;
-    return 0;
+
+    // printing the final result
+    int final = Win();
+    if(final > 0) cout << "Human Won!!" << endl;
+    else if(final < 0) cout << "Computer Won!!" << endl;
+    else cout << "The match has been tied!!" << endl;
 }
